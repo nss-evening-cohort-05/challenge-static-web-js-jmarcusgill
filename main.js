0812df7f53c19ef4b444a552b860510eb25e15f0
jmarcusgill <jmarcusgill@gmail.com>
2017-02-11 13:12:11 -0600

    declared variables from html

---
+++ main.js
@@ -0,0 +1,4 @@
+// Grab and declare HTML elements
+var treeHeight = document.getElementById("height")
+var treeCharacter = document.getElementById("character")
+var growButton = document.getElementById("growIt")
