523c205aa2d54bccf1d138b5dfe1a1f74be15dc9
jmarcusgill <jmarcusgill@gmail.com>
2017-02-11 13:24:40 -0600

    added event listener on enter key and grow button

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 var treeHeight = document.getElementById("height")
 var treeCharacter = document.getElementById("character")
 var growButton = document.getElementById("growIt")
+
+// Event Listener for button
+growButton.addEventListener("click", validation)
+
+// If Enter Key pressed, run validation function
+document.addEventListener("keydown", function (e) {
+  if (e.keycode === "13") {
+    validation();
+  }
+});
+
+validation();
