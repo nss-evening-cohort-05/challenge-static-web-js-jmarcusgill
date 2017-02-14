1007e0c321ca25dd95eed1538f19cf7a95aa4872
jmarcusgill <jmarcusgill@gmail.com>
2017-02-13 18:00:17 -0600

    I beleive this completes the assignment

--- main.js
+++ main.js
@@ -3,14 +3,41 @@ var treeHeight = document.getElementById("height")
 var treeCharacter = document.getElementById("character")
 var growButton = document.getElementById("growIt")
 
-// Event Listener for button
-growButton.addEventListener("click", validation)
 
-// If Enter Key pressed, run validation function
-document.addEventListener("keydown", function (e) {
-  if (e.keycode === "13") {
+
+
+// Event Listener for button
+growButton.addEventListener("click", validation);
+
+//Event Listener for EnterKey
+window.addEventListener("keypress", checkEnter);
+
+//Function for the keypress
+function checkEnter (e) {
+  if (e.keyCode === 13) {
     validation();
   }
-});
+};
 
-validation();
+//Validation Function that checks to see if both inputs are filled and plugs info into myTree object
+function validation() {
+  var myTree = {
+    height: treeHeight.value,
+    character: treeCharacter.value
+  };
+  if (myTree.height === "" || myTree.character === "") {
+    alert("Please fill out the information");
+  } else {
+    console.log("Here Be Ye Tree!")
+    makeTreeGrow(myTree);
+  }
+};
+
+//Function that makes tree grow
+function makeTreeGrow(myTree) {
+  for (var i = 0; i < myTree.height; i++) {
+    var numOfChar = 2*i+1;
+    var numOfSpace = (myTree.height-i);
+    console.log(" ".repeat(numOfSpace), myTree.character.repeat(numOfChar));
+  }
+};
